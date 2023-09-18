a9d4f51f83c244ba81f2eb29ca9f82783d922d70
Galds <96495359+G4lds@users.noreply.github.com>
2023-09-18 20:35:54 -0300

    primeiro teste

---
+++ Game/Main/Enemy.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var vida = 10
+	var ataque = 10
+	print("enemy starting")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Game/Main/Player.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var vida = 10
+	var ataque = 10
+	print("player starting")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print("teste")
+	pass
